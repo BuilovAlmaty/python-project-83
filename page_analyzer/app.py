b9f123d9b2768ccf90573b63926a6e5f0a704f70
Alexey Builov <builovalmaty@gmail.com>
2025-12-04 22:46:58 +0500

    first deploy

---
+++ page_analyzer/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__, template_folder="../templates")
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.get("/")
+def home():
+    return render_template("home1.html")
